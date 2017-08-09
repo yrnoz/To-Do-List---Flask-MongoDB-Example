582f9ceacf3342dd39bab0b5ac4c138fe3ea2702
Heather Shapiro <hshapiro93@gmail.com>
2017-08-09 11:49:08 -0400

    update config file

--- test.py
+++ test.py
@@ -110,7 +110,7 @@ def about():
 	return render_template('credits.html',t=title,h=heading)
 
 if __name__ == "__main__":
-    app.run(debug=True)
-# Careful with the debug mode..
+    app.run()
+
 
 
